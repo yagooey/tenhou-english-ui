856b7e21410b7da1e00a70c2d59f269e976021b8
Zefiris <mai@ryuzu.io>
2017-08-01 23:39:04 +0800

    Initial commit

---
+++ content.js
@@ -0,0 +1,313 @@
+const translationTableExact = {
+    // Main
+    '天鳳 / Web版': 'Tenhou / Web version',
+    '【入力方法の調整を行っています】': 'Input method has been successfully changed',
+    '希望の入力方法ではない場合には': 'If your desired input method is not chosen, ',
+    '「設定」から変更をお願いいたします。': 'you can change it via settings.',
+    '新規ID': 'New ID',
+    'フリテン': 'Furiten',
+    '男': 'Male',
+    '女': 'Female',
+    '設定': 'Settings',
+    'ロビーの移動': 'Change lobby',
+    'ツモ': 'Tsumo',
+    'ロン': 'Ron',
+    'リーチ': 'Riichi',
+    '拔き': 'Nuki',
+    '流局': 'Ryuukyoku',
+    'カン': 'Kan',
+    '終了': 'Quit',
+    '視 ': 'View pt. ',
+    '局 ': 'Round ',
+    '巡 ': 'Turn ',
+    'ランキング戦': 'Ranked Play',
+    '(段位戦、雀荘戦)': '(Ranked, Parlor)',
+    'イベント会場１': 'Event Venue 1',
+    '(公式イベントで使用します)': '(Official events)',
+    'イベント会場２': 'Event Venue 2',
+    'その他': 'Others',
+    '(個室番号を指定してロビーを移動します)': '(Go to a private lobby)',
+    'ロビーの新規作成': 'Create lobby',
+    '(天鳳サイトの個室作成ページを開きます)': '(Link to the lobby creation page)',
+    // Settings
+    '環境': 'Environment',
+    ' / 設定': ' / Settings',
+    '画面方向:回転': 'Screen orientation:Rotate',
+    '※アプリ版でのみご利用いただけます': '※Only applicable in App version',
+    '配信ID保護': 'ID protection for live stream',
+    '※ログイン画面のID入力を非表示にします': '※Makes your ID hidden on the login page',
+    '入力補助:Default': 'Input Assist: Default',
+    '入力補助:3TAP': 'Input Assist: 3TAP',
+    '入力補助:2TAP': 'Input Assist: 2TAP',
+    '※縦画面のみで表示されます': '※Only displayed when in portrait view',
+    '牌山表示': 'Show wall',
+    'SEなし': 'No SE',
+    '卓': 'Table',
+    '標準の画像': 'Use default BG image',
+    '画像URL:': 'Image URL:',
+    '牌': 'Tile',
+    '牌背色:': 'Tile Colour',
+    // Lobby
+    '●アドレスバーを小さくするには下にスクロールしてからゆっくり上にスクロールします(機種依存あり)●OK/パス/ツモ切りは右クリックまたはダブルタップ※予告なく仕様が変更される場合があります': 'To make the address bar smaller, scroll down, then slowly scroll up (depends on the device). OK/Pass/Tsumogiri is right click or double tap. Specifications are subject to change without notice.',
+    '接続:': 'Online:',
+    '待機:': 'Idle:',
+    '終局:': 'Last:',
+    '有効期限:': 'Expire: ',
+    '段級位を取得するには': 'Please register an ID in order',
+    '「新規ID」登録が必要です': 'to be qualified for rankings',
+    'テストプレイ': 'Test Play',
+    '対戦': 'Play',
+    '予約': 'Queue',
+    '1位': '1st',
+    '2位': '2nd',
+    '3位': '3rd',
+    '4位': '4th',
+    '四麻:': '4P',
+    '三麻:': '3P',
+    '東風+4局サドンデス': 'east + 4 rounds of sudden death',
+    'ウマ': 'Uma',
+    '1本場': 'Honba',
+    '後': 'after',
+    '明槓ドラ': 'Mindora',
+    '喰断': 'Kuitan',
+    '東西場': 'Tonshaba',
+    '和了止め': 'Agariyame',
+    '聴牌止め': 'Tenpaiyame',
+    '月間戦績': 'Monthly Stats',
+    '通算': 'Raw',
+    '得点': 'Points',
+    '順位': 'Placement',
+    '収支': 'Income',
+    '祝儀': 'Chips',
+    '平均': 'Average',
+    'トップ': 'Top rt',
+    '連対率': 'Rentai',
+    'ラス率': 'Last rt',
+    '総合': 'Total',
+    '対戦数': '# of games',
+    '※段位戦４人打ち合算戦績': '※Summary of ranked 4P games',
+    '※段位戦３人打ち合算戦績': '※Summary of ranked 3P games',
+    '※表示する対戦ルールを選択してください': 'Choose a game type to display its stats',
+    '通算戦績': 'Raw Stats',
+    '１位率': '1st rt',
+    '２位率': '2nd rt',
+    '３位率': '3rd rt',
+    '４位率': '4th rt',
+    '飛び率': 'Bust rt',
+    '平均得点': 'Avg points',
+    '平均順位': 'Avg placing',
+    '平均収支': 'Avg income',
+    '平均祝儀': 'Avg chips',
+    '和了率': 'Win rt',
+    '放銃率': 'Deal-in rt',
+    '副露率': 'Call rt',
+    '立直率': 'Riichi rt',
+    '牌譜': 'Replay',
+    '牌譜 | ': 'Replay | ',
+    '観戦': 'Spectate',
+    '観戦 | ': 'Watch | ',
+    'ヘルプ': 'Help',
+    '牌理': 'Analyser',
+    '料金のお支払い': 'Pay membership fee',
+    '牌譜/観戦URLを入力してください': 'Enter URL of replay or live game',
+    'ログイン画面の「新規ID」からIDを取得してください': 'Please first register an ID from the login page',
+    '●「長押し」または「右クリック」から別タブで開く/URLのコピーなどが行えます': 'Long press or right click to bring up the context menu.',
+    '●「長押し」または「右クリック」から別タブで開く/URLのコピーなどが行えます●観戦は５分遅れ': 'Long press or right click to bring up the context menu. Games are delayed five minutes in spectating mode.',
+    '観戦可能な対戦はありません': 'There are no matches that can be watched.',
+    '画像や音声がロードされていて正常に描画/再生可能かを確認するための機能です。もし異常があればゲームの進行に支障が出る場合がありますので、このページをリロードしてください。': 'You may use this feature to test whether image and sound files can be properly loaded. If there exists any abnormality which may hinder the progress of the game, please refresh this page.',
+    '上級卓の入場条件(１級以上または有効期限60日以上)を満たしていません※七段R2000以上は入場できません': 'Must be at least 1 Kyuu to enter Joukyuu. ※Not available after 7 Dan & R2000',
+    '特上卓の入場条件(四段R1800以上を満たしていません': 'Must be at least 4 Dan & R1800 to enter Tokujou.',
+    '雀荘戦の入場条件(四段R1800以上の有料会員)を満たしていません': 'Must be at least 4 Dan & R1800 to enter Janso. Not available to free players.',
+    '鳳凰卓の入場条件(七段R2000以上の有料会員)を満たしていません': 'Must be at least 7 Dan & R2000 to enter Houou. Not available to free players.',
+    // Game
+    '對局': 'Start',
+    '鳴きなし': 'No call',
+    '自動理牌': 'Sort',
+    '不聴宣言': 'Noten',
+    '自動和了': 'Auto ron',
+    'ツモ切り': 'Tsumogiri',
+    'ポン': 'Pon',
+    'チー': 'Chii',
+    '槓': 'Kan',
+    '終局': 'End',
+    // Replay & Spectating
+    '待ち': 'Waits',
+    '手牌': 'Hand',
+    '牌山': 'Wall',
+    '(匿名表示 ON)': '(ID Display ON)',
+    '(匿名表示 OFF)': '(ID Display OFF)',
+}
+
+const translationTablePartial = {
+    '新人': '新人',
+    '９級': '９級',
+    '８級': '８級',
+    '７級': '７級',
+    '６級': '６級',
+    '５級': '５級',
+    '４級': '４級',
+    '３級': '３級',
+    '２級': '２級',
+    '１級': '１級',
+    '初段': '初段',
+    '二段': '二段',
+    '三段': '三段',
+    '四段': '四段',
+    '五段': '五段',
+    '六段': '六段',
+    '七段': '七段',
+    '八段': '八段',
+    '九段': '九段',
+    '十段': '十段',
+    '四般東喰赤': '4, Gen, East, Open, Red',
+    '四般東喰赤速': '4, Gen, East, Open, Red, Fast',
+    '四般南喰赤': '4, Gen, South, Open, Red',
+    '四般南喰赤速': '4, Gen, South, Open, Red, Fast',
+    '三般東喰赤': '3, Gen, East, Open, Red',
+    '三般東喰赤速': '3, Gen, East, Open, Red, Fast',
+    '三般南喰赤': '3, Gen, South, Open, Aka',
+    '三般南喰赤速': '3, Gen, South, Open, Aka Fast',
+    '四上東喰赤': '4, Inter, East, Open, Red',
+    '四上東喰赤速': '4, Inter, East, Open, Red, Fast',
+    '四上南喰赤': '4, Inter, South, Open, Aka',
+    '四上南喰赤速': '4, Inter, South, Open, Aka Fast',
+    '三上東喰赤': '3, Inter, East, Open, Red',
+    '三上東喰赤速': '3, Inter, East, Open, Red, Fast',
+    '三上南喰赤': '3, Inter, South, Open, Aka',
+    '三上南喰赤速': '3, Inter, South, Open, Aka Fast',
+    '四特東喰赤': '4, Expert, East, Open, Red',
+    '四特東喰赤速': '4, Expert, East, Open, Red, Fast',
+    '四特南喰赤': '4, Expert, South, Open, Aka',
+    '四特南喰赤速': '4, Expert, South, Open, Aka Fast',
+    '三特東喰赤': '3, Expert, East, Open, Red',
+    '三特東喰赤速': '3, Expert, East, Open, Red, Fast',
+    '三特南喰赤': '3, Expert, South, Open, Aka',
+    '三特南喰赤速': '3, Expert, South, Open, Aka Fast',
+    '四鳳東喰赤': '4, Master, East, Open, Red',
+    '四鳳東喰赤速': '4, Master, East, Open, Red, Fast',
+    '四鳳南喰赤': '4, Master, South, Open, Aka',
+    '四鳳南喰赤速': '4, Master, South, Open, Aka Fast',
+    '三鳳東喰赤': '3, Master, East, Open, Red',
+    '三鳳東喰赤速': '3, Master, East, Open, Red, Fast',
+    '三鳳南喰赤': '3, Master, South, Open, Aka',
+    '三鳳南喰赤速': '3, Master, South, Open, Aka Fast',
+    '四麻上級卓': '4 players, Intermediate',
+    '三麻上級卓': '3 players, Intermediate',
+    '四麻特上卓': '4 players, Expert',
+    '三麻特上卓': '3 players, Expert',
+    '四麻雀荘戦': '4 players, Parlor',
+    '三麻雀荘戦': '3 players, Parlor',
+    '四麻鳳凰卓': '4 players, Master',
+    '三麻鳳凰卓': '3 players, Master',
+    // '四琥東速祝０': '',
+    // '四琥東速祝２': '',
+    // '四琥東速祝５': '',
+    // '四琥南祝０': '',
+    // '四琥南祝２': '',
+    // '四琥南祝５': '',
+    // '三琥東速祝５': '',
+    // '三琥南祝５': '',
+    '巡目': ' Turn',
+}
+
+const translationTableTooltip = {
+    '新人': 'Rookie',
+    '９級': '9kyuu',
+    '８級': '8kyuu',
+    '７級': '7kyuu',
+    '６級': '6kyuu',
+    '５級': '5kyuu',
+    '４級': '4kyuu',
+    '３級': '3kyuu',
+    '２級': '2kyuu',
+    '１級': '1kyuu',
+    '初段': '1dan',
+    '二段': '2dan',
+    '三段': '3dan',
+    '四段': '4dan',
+    '五段': '5dan',
+    '六段': '6dan',
+    '七段': '7dan',
+    '八段': '8dan',
+    '九段': '9dan',
+    '十段': '10dan',
+    '四般東喰赤速': '4 players, General, East round, Open tanyao, Red dora, Fast',
+    '四般南喰赤': '4 players, General, East + South round, Open tanyao, Red dora',
+    '四般南喰赤速': '4 players, General, East + South round, Open tanyao, Red dora, Fast',
+    '三般東喰赤': '3 players, General, East round, Open tanyao, Red dora',
+    '三般東喰赤速': '3 players, General, East round, Open tanyao, Red dora, Fast',
+    '三般南喰赤': '3 players, General, East + South round, Open tanyao, Red dora',
+    '三般南喰赤速': '3 players, General, East + South round, Open tanyao, Red dora, Fast',
+    '四上東喰赤': '4 players, Intermediate, East round, Open tanyao, Red dora',
+    '四上東喰赤速': '4 players, Intermediate, East round, Open tanyao, Red dora, Fast',
+    '四上南喰赤': '4 players, Intermediate, East + South round, Open tanyao, Red dora',
+    '四上南喰赤速': '4 players, Intermediate, East + South round, Open tanyao, Red dora, Fast',
+    '三上東喰赤': '3 players, Intermediate, East round, Open tanyao, Red dora',
+    '三上東喰赤速': '3 players, Intermediate, East round, Open tanyao, Red dora, Fast',
+    '三上南喰赤': '3 players, Intermediate, East + South round, Open tanyao, Red dora',
+    '三上南喰赤速': '3 players, Intermediate, East + South round, Open tanyao, Red dora, Fast',
+    '四特東喰赤': '4 players, Expert, East round, Open tanyao, Red dora',
+    '四特東喰赤速': '4 players, Expert, East round, Open tanyao, Red dora, Fast',
+    '四特南喰赤': '4 players, Expert, East + South round, Open tanyao, Red dora',
+    '四特南喰赤速': '4 players, Expert, East + South round, Open tanyao, Red dora, Fast',
+    '三特東喰赤': '3 players, Expert, East round, Open tanyao, Red dora',
+    '三特東喰赤速': '3 players, Expert, East round, Open tanyao, Red dora, Fast',
+    '三特南喰赤': '3 players, Expert, East + South round, Open tanyao, Red dora',
+    '三特南喰赤速': '3 players, Expert, East + South round, Open tanyao, Red dora, Fast',
+    '四鳳東喰赤': '4 players, Master, East round, Open tanyao, Red dora',
+    '四鳳東喰赤速': '4 players, Master, East round, Open tanyao, Red dora, Fast',
+    '四鳳南喰赤': '4 players, Master, East + South round, Open tanyao, Red dora',
+    '四鳳南喰赤速': '4 players, Master, East + South round, Open tanyao, Red dora, Fast',
+    '三鳳東喰赤': '3 players, Master, East round, Open tanyao, Red dora',
+    '三鳳東喰赤速': '3 players, Master, East round, Open tanyao, Red dora, Fast',
+    '三鳳南喰赤': '3 players, Master, East + South round, Open tanyao, Red dora',
+    '三鳳南喰赤速': '3 players, Master, East + South round, Open tanyao, Red dora, Fast',
+}
+
+const exactPhrases = Object.values(translationTableExact);
+const partialPhrases = Object.keys(translationTablePartial);
+const textList = [];
+
+const partialMatch = (originalText) => {
+    let key;
+    for (let i = 0; i < partialPhrases.length; i++) {
+        if (originalText.includes(partialPhrases[i])) {
+            key = partialPhrases[i];
+        }
+    }
+    return key;
+};
+
+setInterval(() => {
+    const elements = document.getElementsByTagName('*');
+
+    for (let i = 0; i < elements.length; i++) {
+        for (let j = 0; j < elements[i].childNodes.length; j++) {
+            let node = elements[i].childNodes[j];
+            if (node.nodeType === 3) {
+                let originalText = node.nodeValue;
+                if (translationTableExact[originalText]) {
+                    elements[i].replaceChild(document.createTextNode(translationTableExact[originalText]), node);
+                } else if (partialMatch(originalText)) {
+                    let partialMatchKey = partialMatch(originalText);
+                    if (partialMatchKey) {
+                        let newText = originalText.replace(partialMatchKey, translationTablePartial[partialMatchKey]);
+                        elements[i].replaceChild(document.createTextNode(newText), node);
+                        if (translationTableTooltip[partialMatchKey]) {
+                            if (elements[i].tagName.toLowerCase() === 'span') {
+                                elements[i].parentElement.style.overflow = 'hidden';
+                                elements[i].parentElement.setAttribute('title', translationTableTooltip[partialMatchKey]);
+                            } else {
+                                elements[i].style.overflow = 'hidden';
+                                elements[i].setAttribute('title', translationTableTooltip[partialMatchKey]);
+                            }
+                        }
+                    }
+                } else if (textList.indexOf(originalText) < 0 && exactPhrases.indexOf(originalText) < 0 && originalText.trim().length > 0 && originalText.length < 255) {
+                    textList.push(originalText);
+                    console.log(originalText);
+                }
+            }
+        }
+    };
+}, 16);
