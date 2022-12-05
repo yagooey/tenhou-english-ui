70117aa9e47e08507a07480c51e4c0b088e2c20c
AZPS <github@londonanalytics.info>
2022-12-05 13:10:02 +0000

    seem to have working background service worker for chrome, as well as background.js for firefox

--- package/serviceworker.js
+++ package/serviceworker.js
@@ -2,37 +2,36 @@
 /* globals chrome */
 
 chrome.runtime.onInstalled.addListener(function(object) {
+    
+    // Clear all rules to ensure only our expected rules are set
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        let rule = {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: {hostSuffix: 'tenhou.net'},
+            }),
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: {hostSuffix: 'ron2.jp'},
+            })
+          ],
+          actions: [new chrome.declarativeContent.ShowAction()], // show addon icon for options screen
+        };
+        chrome.declarativeContent.onPageChanged.addRules([rule]);
+        // console.log('icon should have been added now'); // TODO icon still not added, don't know why
+    });
+
     // show options screen on install
     if (chrome.runtime.OnInstalledReason.INSTALL === object.reason) {
         chrome.runtime.openOptionsPage();
     }
 });
 
-/*
-// show the addon button for one tab
-function showIconForTab(tab) {
-    if (tab.url.includes('tenhou.net') || tab.url.includes('ron2.jp')) {
-        chrome.pageAction.show(tab.id);
-    }
-}
-
-// on initialisation, show the addon button for all tenhou tabs
-chrome.tabs.query({ url: '*://tenhou.net/*|*://ron2.jp/*' }, (tabs) => {
-    for (let tab of tabs) {
-        showIconForTab(tab);
-    }
-});
-
-chrome.tabs.onUpdated.addListener((id, changeInfo, tab) => showIconForTab(tab));
-*/
-
 function updateTileset(items) {
-    let newrule = '';
+    let newrule = [];
     let set = items.tileset;
     let sub = `https://mahjong.ie/files/tiles/tiles.php?set=${set}&id=\\1&w=\\20`
-    console.log(sub);
     if (set !== 'DEFAULT' ) {
-        newrule = {
+        newrule = [{
             'id': 1,
             priority: 1,
             action: {
@@ -41,16 +40,15 @@ function updateTileset(items) {
             },
             condition: {
                 resourceTypes: ["image"],
-                regexFilter: "^https://cdn.tenhou.net/./img/vieww([0-4])000*([1-9][0-9]*).png$"
-                //regexFilter: "^https://cdn\\.tenhou\\.net/*/img/vieww([0-4])000*([1-9][0-9]*)\\.png$"
+                regexFilter: "^https://cdn\.tenhou\.net/./img/vieww([0-4])000*([1-9][0-9]*)\.png$"
             }
-        };
+        }];
     }
 
     chrome.declarativeNetRequest.updateDynamicRules(
         {
             removeRuleIds: [1],
-            addRules: [newrule],
+            addRules: newrule,
         }
     );
 }
@@ -62,3 +60,7 @@ chrome.runtime.onMessage.addListener(updateTileset);
 
 // on load of extension
 chrome.storage.local.get({ tileset: 'DEFAULT' }, (items) => updateTileset(items));
+
+chrome.action.disable();
+
+
